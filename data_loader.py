c101b58e4a8cf453ec41a783623ce016c520005e
iteratorlee <yan.l@pku.edu.cn>
2020-09-22 15:45:55 +0800

    data_loader v1
    1. add basic functions in utils.py
    2. finish data loader version 1

--- data_loader.py
+++ data_loader.py
@@ -3,23 +3,84 @@ import sys
 import json
 import numpy as np
 
+from collections import defaultdict
+
 from conf import LumosConf
+from utils import read_csv, get_json_value, mget_json_values, encode_timestamp
+
+
+class RecordEntry(object):
+    '''
+    A record entry of running a workload on a concrete configuration
+    '''
+    def __init__(self, inst_type, scale, metrics, jct, ts):
+        '''
+        params:
+        @scale: size of input
+        @metrics: low-level system metrics data
+        @jct: job completion time
+        @ts: timestamp
+        '''
+        self.inst_type = inst_type
+        self.scale = scale
+        self.metrics = metrics
+        self.jct = jct
+        self.ts = ts
+    
+
+    def as_vector(self):
+        '''
+        turn this record to a vector that can be fed into a prediction model
+        '''
+        return []
 
 
 class DataLoader(object):
-
+    '''
+    Load training or testing data
+    '''
     def __init__(self):
-        conf = LumosConf()
-        ds_root_pth = conf.get('dataset', 'path')
-
+        self.conf = LumosConf()
+        self.ds_root_pth = self.conf.get('dataset', 'path')
+        self.vendor_cnt = self.conf.get('dataset', 'vendor_cnt')
+        self.__data = defaultdict(lambda: defaultdict(lambda: []))
 
     def load_data(self):
-        pass
+        
+        def is_vendor(v):
+            return '.' not in v
+
+        for vendor in os.listdir(self.ds_root_pth):
+            if not is_vendor(vendor): continue
+            pth1 = os.path.join(self.ds_root_pth, vendor)
+            for inst_type in os.listdir(pth1):
+                pth2 = os.path.join(pth1, inst_type)
+                for w in os.listdir(pth2):
+                    [scale, _] = w.strip().split('_')[-2:]
+                    workload = '_'.join(w.strip().split('_')[:2])
+                    pth3 = os.path.join(pth2, w)
+                    pth_report = os.path.join(pth3, 'report.json')
+                    pth_metrics = os.path.join(pth3, 'sar.csv')
+                    [ts, jct] = mget_json_values(pth_report, 'timestamp', 'elapsed_time')
+                    ts = encode_timestamp(ts)
+                    jct = float(jct)
+                    header, metrics = read_csv(pth_metrics)
+                    if not header or not metrics: continue
+                    self.__data[workload][vendor].append(
+                        RecordEntry(inst_type, scale, metrics, jct, ts))
 
 
     def get_data(self):
-        pass
+        return self.__data
 
 
 if __name__ == "__main__":
     data_loader = DataLoader()
+    data_loader.load_data()
+    data = data_loader.get_data()
+    # print(len(data))
+    # print(len(data['hadoop_aggregation']))
+    # print(len(data['hadoop_aggregation']['alibaba']))
+    # print(len(data['hadoop_aggregation']['huawei']))
+    # print(len(data['hadoop_aggregation']['tencent']))
+    # print(len(data['hadoop_aggregation']['ucloud']))
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import json
 
 def singleton(cls):
     _instance = {}
@@ -8,3 +9,50 @@ def singleton(cls):
         return _instance[cls]
 
     return inner
+
+
+def read_csv(csv_file_path):
+    with open(csv_file_path, 'r') as fd:
+        lines = fd.readlines()
+        header, content = lines[0], lines[1:]
+        content = [line.strip().split(',') for line in content]
+        for i in range(len(content)):
+            for j in range(len(content[i]) - 1):
+                ele = content[i][j + 1]
+                if ele == 'lo': # IFACE
+                    content[i][j + 1] = 0
+                elif ele.replace('.', '').isdigit():
+                    content[i][j + 1] = float(ele)
+                else:
+                    return None, None
+        return header, content
+
+
+def get_json_value(json_file_path, *keys):
+    with open(json_file_path, 'r') as fd:
+        data = json.load(fd)
+        tmp = data[keys[0]]
+        if len(keys) > 1:
+            for i in range(len(keys) - 1):
+                tmp = tmp[keys[i + 1]]
+        return tmp
+
+
+def mget_json_values(json_file_path, *key_arr):
+    with open(json_file_path, 'r') as fd:
+        data = json.load(fd)
+        ret = []
+        for keys in key_arr:
+            if isinstance(keys, list):
+                tmp = data[keys[0]]
+                if len(keys) > 1:
+                    for i in range(len(keys) - 1):
+                        tmp = tmp[keys[i + 1]]
+                    ret.append(tmp)
+            elif isinstance(keys, str):
+                ret.append(data[keys])
+        return ret
+
+
+def encode_timestamp(ts):
+    return 0
