7a89215f5b9eeae3f4a61b94faea3221bf3dacab
iteratorlee <yan.l@pku.edu.cn>
2020-09-25 15:13:14 +0800

    remove useless TopEncoder class

--- encoder.py
+++ encoder.py
@@ -14,35 +14,6 @@ from data_loader import DataLoader
 from third_party.keras_lstm_vae.lstm_vae import create_lstm_vae
 
 
-class TopEncoder(object):
-    '''
-    every workload should have a LSTM autoencoder, which is trained seperated from others
-    '''
-    def __init__(self, to_load_workloads=1):
-        '''
-        params:
-        @to_load_workloads
-            type: int/list
-            the encoders that needs to be loaded
-        '''
-        self.to_load_workloads = to_load_workloads
-        self.load_encoders()
-
-
-    def load_encoders(self):
-        '''
-        TODO: load_encoders from TF dump files
-        '''
-        pass
-
-        
-    def encode_metrics(self, metrics):
-        '''
-        TODO: encode the metrics
-        '''
-        return metrics
-
-
 if __name__ == "__main__":
     conf = LumosConf()
     dump_pth = conf.get('dataset', 'dump_pth')
