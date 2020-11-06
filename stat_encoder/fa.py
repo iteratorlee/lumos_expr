60495cfe57a5f93ae4efed23b75ab3bd30106b1a
iteratorlee <yan.l@pku.edu.cn>
2020-11-06 16:40:03 +0800

    updata fa encoder

--- stat_encoder/fa.py
+++ stat_encoder/fa.py
@@ -4,6 +4,22 @@ class FAEncoder(object):
     '''
     def __init__(self):
         # TODO: fa encoder
+        self.__data = None
+        self.A = None
+        pass
+
+
+    def fit(self, data):
+        '''
+        params:
+        @data: array-like object with the following structure:
+            [
+                [x00, x01, x02, ..., x0p-1],
+                ...,
+                [xn-10, xn-11, xn-12, ..., xn-1p-1]
+            ]
+            in which there are n samples that each has p features
+        '''
         pass
 
 
