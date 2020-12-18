06209fc015015e6c926ee651038345bfc809ac8e
iteratorlee <yan.l@pku.edu.cn>
2020-12-18 14:40:34 +0800

    add feat selection

---
+++ feature_select.py
@@ -0,0 +1,36 @@
+import json
+import random
+import numpy as np
+from conf import LumosConf
+from data_loader_ordinal import DataLoaderOrdinal
+
+
+def cal_cor(metrics):
+    return np.cov(metrics.T)
+
+
+def ana_metrics(metrics_data):
+    stat_res = []
+    for metrics in metrics_data:
+        cor = cal_cor(metrics)
+        stat_res.append(cor)
+    return stat_res
+
+
+def select_features(metrics_data):
+    return []
+
+
+if __name__ == "__main__":
+    conf = LumosConf()
+    dump_pth = conf.get('dataset', 'dump_pth_ordinal_with_truc_v1')
+    dataloader = DataLoaderOrdinal(dump_pth=dump_pth)
+    dataloader.load_data_by_interval(interval=1)
+    data = dataloader.get_data()
+    to_select_scale = 'large'
+    metrics_data = []
+    for wl, wl_data in data['1'].items():
+        scale_data = wl_data[to_select_scale]
+        metrics_data.append(random.sample(scale_data, 1)[0].metrics)
+    # feature_idxes = select_features(metrics_data)
+    ana_metrics(metrics_data)
