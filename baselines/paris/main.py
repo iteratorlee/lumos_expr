a1c61ed713deb91cdfd2dce5a510cde1df721d5f
jind5182 <jindpku@163.com>
2021-01-06 14:57:51 +0800

    paris update

--- baselines/paris/main.py
+++ baselines/paris/main.py
@@ -4,233 +4,190 @@ import numpy as np
 import json, os
 from sklearn.ensemble import RandomForestRegressor  
 
-features = {}
-confs = {}
-instances = []
-Path = '/Users/macpro/Desktop/lumos-dataset/'
-vendors = ['alibaba/1 second/', 'huawei/1 second/', 'tencent/1 second/', 'aws/']
-vendors2 = ['alibaba/5 second/', 'huawei/5 second/', 'tencent/5 second/'] 
-for vendor in vendors:
-	for instance in os.listdir(Path + vendor):
-		if instance[0] == '.':
-			continue
-		path = Path + vendor + instance
-		instances.append(instance)
-		for bench in os.listdir(path):
-			if bench[0] == '.' or bench[-1] in ['4', '5', '6']:
+# 'small', 'large' or 'all'
+train_scale = 'all'
+predict_scale = 'all'
+top = 5
+normalize_rate = 30
+# choose 2 instance types as reference, use runtime on them to predict runtime on other VMs
+reference_instances = ['hfr6.2xlarge', 's6.2xlarge.2']
+n_estimators = 10
+
+def get_features():
+	"""
+	Get performance metrics of all workloads on all VMs
+	Change Path to lumos-dataset
+	"""
+	features = {}
+	confs = {}
+	with open('conf/detail_conf.json') as f:
+		confs = json.load(f)
+	Path = '/Users/macpro/Desktop/lumos-dataset/'
+	vendors = ['alibaba/1 second/', 'huawei/1 second/', 'tencent/1 second/', 'aws/']
+	vendors2 = ['alibaba/5 second/', 'huawei/5 second/', 'tencent/5 second/'] 
+	for vendor in vendors:
+		for instance in os.listdir(Path + vendor):
+			if instance[0] == '.':
 				continue
-			with open(path + '/' + bench + '/report.json') as f:
-				result = json.load(f)
-				if not bench in features:
-					features[bench] = {instance: [float(result['elapsed_time'])]}
-				else:
-					features[bench][instance] = [float(result['elapsed_time'])]
-			with open(path + '/' + bench + '/sar.csv') as f:
-				lines = f.readlines()[1:-1]
-				l = len(lines)
-				tmp = lines[int(l/4)].strip().split(',')[1:]
-				tmp.remove('lo')
-				tmp.extend(lines[int(2*l/4)].strip().split(',')[1:])
-				tmp.remove('lo')
-				tmp.extend(lines[int(3*l/4)].strip().split(',')[1:])
-				tmp.remove('lo')
-				features[bench][instance].extend(tmp)
-for vendor in vendors2:
-	for instance in os.listdir(Path + vendor):
-		if instance[0] == '.':
-			continue
-		path = Path + vendor + instance
-		for bench in os.listdir(path):
-			if bench[0] == '.' or bench[-1] in ['4', '5', '6'] or (('hadoop' in bench or 'spark' in bench) and ('tiny' in bench or 'small' in bench)):
+			path = Path + vendor + instance
+			for bench in os.listdir(path):
+				if bench[-2:] == '_2' or bench[-2:] == '_3':
+					continue
+				if bench[0] == '.' or bench[-1] in ['4', '5', '6']:
+					continue
+				with open(path + '/' + bench + '/report.json') as f:
+					result = json.load(f)
+					if not bench in features:
+						features[bench] = {instance: [float(result['elapsed_time'])]}
+					else:
+						features[bench][instance] = [float(result['elapsed_time'])]
+				with open(path + '/' + bench + '/sar.csv') as f:
+					lines = f.readlines()[1:-1]
+					l = len(lines)
+					tmp = lines[int(l/4)].strip().split(',')[1:]
+					tmp.remove('lo')
+					tmp.extend(lines[int(2*l/4)].strip().split(',')[1:])
+					tmp.remove('lo')
+					tmp.extend(lines[int(3*l/4)].strip().split(',')[1:])
+					tmp.remove('lo')
+					features[bench][instance].extend(tmp)
+	for vendor in vendors2:
+		for instance in os.listdir(Path + vendor):
+			if instance[0] == '.':
 				continue
-			with open(path + '/' + bench + '/report.json') as f:
-				result = json.load(f)
-				if not bench in features:
-					features[bench] = {instance: [float(result['elapsed_time'])]}
-				else:
-					features[bench][instance] = [float(result['elapsed_time'])]
-			with open(path + '/' + bench + '/sar.csv') as f:
-				lines = f.readlines()[1:-1]
-				l = len(lines)
-				tmp = lines[int(l/4)].strip().split(',')[1:]
-				tmp.remove('lo')
-				tmp.extend(lines[int(2*l/4)].strip().split(',')[1:])
-				tmp.remove('lo')
-				tmp.extend(lines[int(3*l/4)].strip().split(',')[1:])
-				tmp.remove('lo')
-				features[bench][instance].extend(tmp)
-with open('detail_conf.json') as f:
-	confs = json.load(f)
+			path = Path + vendor + instance
+			for bench in os.listdir(path):
+				if bench[-2:] == '_2' or bench[-2:] == '_3':
+					continue
+				if bench[0] == '.' or bench[-1] in ['4', '5', '6'] or (('hadoop' in bench or 'spark' in bench) and ('tiny' in bench or 'small' in bench)):
+					continue
+				with open(path + '/' + bench + '/report.json') as f:
+					result = json.load(f)
+					if not bench in features:
+						features[bench] = {instance: [float(result['elapsed_time'])]}
+					else:
+						features[bench][instance] = [float(result['elapsed_time'])]
+				with open(path + '/' + bench + '/sar.csv') as f:
+					lines = f.readlines()[1:-1]
+					l = len(lines)
+					tmp = lines[int(l/4)].strip().split(',')[1:]
+					tmp.remove('lo')
+					tmp.extend(lines[int(2*l/4)].strip().split(',')[1:])
+					tmp.remove('lo')
+					tmp.extend(lines[int(3*l/4)].strip().split(',')[1:])
+					tmp.remove('lo')
+					features[bench][instance].extend(tmp)
+	return features, confs
 
-X = []
-y = []
-benches = {}
-cost = []
+def handle_features(features, confs):
+	"""
+	get train data X, y from original features
+	return costs: cost of instance of i-th train data 
+	       benches: benchname of i-th train data 
+	"""
+	X = []
+	y = []
+	costs = []
+	benches = []
+	for bench, datas in features.items():
+		for instance, data in datas.items():
+			# target instance information
+			tmp = list(confs[instance].values())[0:3]
+			# performance metrics on two reference VMs
+			tmp.extend(datas[reference_instances[0]])
+			tmp.extend(datas[reference_instances[1]])
+			X.append(tmp)
+			# normalized time
+			y.append(data[0]*normalize_rate/datas[reference_instances[0]][0])
+			costs.append(list(confs[instance].values())[3])
+			benches.append(bench)
+	return X, y, costs, benches
 
-for bench, datas in features.items():
-	if bench[-2:] == '_2' or bench[-2:] == '_3':# or not 'large' in bench:
-		continue
-	for instance, data in datas.items():
-		tmp = list(confs[instance].values())[0:3]
-		tmp.extend(datas['hfr6.2xlarge'])
-		tmp.extend(datas['s6.2xlarge.2'])
-		# {'g6.large': 223, 'r6.2xlarge': 225, 'g6.2xlarge': 226, 'hfc6.large': 217, 'hfg6.xlarge': 223, 'hfr6.xlarge': 223, 'hfg6.2xlarge': 225, 'c6.xlarge': 223, 'hfg6.large': 223, 'hfr6.2xlarge': 226, 'c6.large': 217, 'r6.large': 223, 'g6.xlarge': 223, 'hfc6.2xlarge': 226, 'r6.xlarge': 223, 'hfr6.large': 223, 'c6.2xlarge': 226, 'hfc6.xlarge': 223, 'm6.large.8': 223, 'm6.2xlarge.8': 226, 'c6.large.2': 217, 'c6.2xlarge.2': 226, 'c6.2xlarge.4': 225, 'c6.large.4': 223, 's6.xlarge.4': 218, 's6.xlarge.2': 220, 'c6.xlarge.4': 216, 'c6.xlarge.2': 221, 's6.large.4': 214, 's6.2xlarge.4': 226, 's6.large.2': 217, 's6.2xlarge.2': 226, 'm6.xlarge.8': 223, 'c3.large8': 223, 'c3.2xlarge32': 226, 's5.large16': 197, 's5.medium8': 198, 'm5.2xlarge64': 226, 'm5.large32': 223, 'c3.large16': 223, 'c3.2xlarge16': 225, 's5.2xlarge16': 225, 's5.large8': 198, 'm5.medium16': 223, 's5.2xlarge32': 201, 's5.medium4': 192, 'm5.large': 212, 'c5.large': 196, 'r5.xlarge': 210, 'r5.2xlarge': 213, 'm5.2xlarge': 208, 'm5.xlarge': 210, 'c5.2xlarge': 211, 'r5.large': 211, 'c5.xlarge': 211}
-		X.append(tmp)
-		y.append(data[0]/datas['hfr6.2xlarge'][0])
-		cost.append(list(confs[instance].values())[3])
-		if bench[-2:] == '_1':
-			if not bench[:-2] in benches:
-				benches[bench[:-2]] = []
-			benches[bench[:-2]].append(len(y)-1)
-		else:
-			if not bench in benches:
-				benches[bench] = []
-			benches[bench].append(len(y)-1)
+def get_accuracy(train_X, train_y, test_X, test_y, benches, costs, top, same=False):
+	"""
+	calculate jct accuracy and cost accuracy
+	top: top n
+	same: whether the train_data and predict_data are the same, 
+		if true, extract the using one predict_data from train_data
+		else, append two datas on reference VMs from predict_data to train_data 
+	"""
+	total = 0
+	jct_correct = 0	
+	cost_correct = 0
+	rf = RandomForestRegressor(n_estimators=n_estimators, random_state=0) 
+	if not same:
+		rf.fit([t[1] for t in train_X], [t[1] for t in train_y])
+	all_bench = set([benches[t[0]] for t in test_y])
+	for bench in all_bench:
+		if same:
+			rf.fit([t[1] for t in train_X if benches[t[0]] != bench], [t[1] for t in train_y if benches[t[0]] != bench])
+		predict_X = [t[1] for t in test_X if benches[t[0]] == bench]
+		real_y = [t[1] for t in test_y if benches[t[0]] == bench]
+		tmpcosts = [costs[t[0]] for t in test_y if benches[t[0]] == bench]
+		predict_y = rf.predict(predict_X)
+		
+		chooseninstance = np.argmin(np.array(predict_y))
+		result = real_y[chooseninstance]
 
-total = 0
-correct1 = 0	
-correct2 = 0
-print(len(instances))
-print(len(list(benches.keys())))
+		count = 0
+		flag = True
+		for time in real_y:
+			if time < result:
+				count += 1
+			# threshold
+			if count > top:
+				flag = False
+				break
+		if flag:
+			jct_correct += 1
 
-t = 0
-for bench, indexes in benches.items():
-	t += 1
-	print(t, bench)
-	train_X = X[:]
-	train_y = y[:]
-	test_X = []
-	test_y = []
-	tmpcost = []
-	for i in sorted(indexes, reverse=True):
-		del train_X[i]
-		del train_y[i]
-	for i in indexes:
-		test_X.append(X[i])
-		test_y.append(y[i])
-		tmpcost.append(cost[i])
-	rf = RandomForestRegressor(n_estimators=10, random_state=0) 
-	rf.fit(train_X, train_y)
-	predict_y = rf.predict(test_X)
-	print(test_y, predict_y, tmpcost)
-	'''
-	min_index = 0
-	min_result = 9999
-	for k in range(len(predict_y)):
-		if predict_y[k] < min_result:
-			min_result = predict_y[k]
-			min_index = k
-	result = test_y[min_index]
-	count = 0
-	flag1 = True
-	flag2 = True
-	for k in test_y:
-		if k < result:
-			count += 1
-		# threshold
-		if count > 1:
-			flag1 = False
-		if count > 3:
-			flag2 = False
-			break
-	'''
-	min_index = 0
-	min_result = 9999
-	for k in range(len(predict_y)):
-		if predict_y[k]*tmpcost[k] < min_result:
-			min_result = predict_y[k]*tmpcost[k]
-			min_index = k
-	result = test_y[min_index]*tmpcost[min_index]
-	count = 0
-	flag1 = True
-	flag2 = True
-	for i, k in enumerate(test_y):
-		if k*tmpcost[i] < result:
-			count += 1
-		# threshold
-		if count > 1:
-			flag1 = False
-		if count > 3:
-			flag2 = False
-			break
-	
-	total += 1
-	if flag1:
-		correct1 += 1
-	if flag2:
-		correct2 += 1
-print(correct1 / total)
-print(correct2 / total)
+		chooseninstance = np.argmin(np.array(predict_y)*np.array(tmpcosts))
+		result = real_y[chooseninstance] * tmpcosts[chooseninstance]
 
-'''
-train_X = []
-train_y = []
-for bench, indexes in benches.items():
-	if 'small' in bench:
-		for i in indexes:
-			train_X.append(X[i])
-			train_y.append(y[i])
-	
-rf = RandomForestRegressor(n_estimators=10, random_state=0) 
-rf.fit(train_X, train_y)
+		count = 0
+		flag = True
+		for j, time in enumerate(real_y):
+			if time * tmpcosts[j] < result:
+				count += 1
+			# threshold
+			if count > top:
+				flag = False
+				break
+		if flag:
+			cost_correct += 1
 
-for bench, indexes in benches.items():
-	test_X = []
-	test_y = []
-	tmpcost = []
-	if not 'huge' in bench:
-		continue
-	for i in indexes:
-		test_X.append(X[i])
-		test_y.append(y[i])
-		tmpcost.append(cost[i])
-	predict_y = rf.predict(test_X)
-	
-	min_index = 0
-	min_result = 9999
-	for k in range(len(predict_y)):
-		if predict_y[k] < min_result:
-			min_result = predict_y[k]
-			min_index = k
-	result = test_y[min_index]
-	count = 0
-	flag1 = True
-	flag2 = True
-	for k in test_y:
-		if k < result:
-			count += 1
-		# threshold
-		if count >= 6:
-			flag1 = False
-		if count >= 12:
-			flag2 = False
-			break
+		total += 1
+	return jct_correct / total, cost_correct / total
 
-	min_index = 0
-	min_result = 9999
-	for k in range(len(predict_y)):
-		if predict_y[k]*tmpcost[k] < min_result:
-			min_result = predict_y[k]*tmpcost[k]
-			min_index = k
-	result = test_y[min_index]*tmpcost[min_index]
-	count = 0
-	flag1 = True
-	flag2 = True
-	for i, k in enumerate(test_y):
-		if k*tmpcost[i] < result:
-			count += 1
-		# threshold
-		if count >= 6:
-			flag1 = False
-		if count >= 12:
-			flag2 = False
-			break
+if __name__ == '__main__':
+	features, confs = get_features()
+	X, y, costs, benches = handle_features(features, confs)
+	train_X = []; train_y = []
+	test_X = []; test_y = []
+	if train_scale == 'all':
+		train_X = [(i,t) for i, t in enumerate(X) if 'small' in benches[i] or 'large' in benches[i]]
+		train_y = [(i,t) for i, t in enumerate(y) if 'small' in benches[i] or 'large' in benches[i]]
+	else:
+		train_X = [(i,t) for i, t in enumerate(X) if train_scale in benches[i]]
+		train_y = [(i,t) for i, t in enumerate(y) if train_scale in benches[i]]
+	if predict_scale == 'all':
+		test_X = [(i,t) for i, t in enumerate(X) if 'small' in benches[i] or 'large' in benches[i]]
+		test_y = [(i,t) for i, t in enumerate(y) if 'small' in benches[i] or 'large' in benches[i]]
+	else:
+		test_X = [(i,t) for i, t in enumerate(X) if predict_scale in benches[i]]
+		test_y = [(i,t) for i, t in enumerate(y) if predict_scale in benches[i]]
+	if train_scale != predict_scale:
+		jct_accuracy, cost_accuracy = get_accuracy(train_X, train_y, test_X, test_y, benches, costs, top)
+	else:
+		jct_accuracy, cost_accuracy = get_accuracy(train_X, train_y, test_X, test_y, benches, costs, top, True)
+	print(jct_accuracy, cost_accuracy)
 
-	total += 1
-	if flag1:
-		correct1 += 1
-	if flag2:
-		correct2 += 1
-print(correct1 / total)
-print(correct2 / total)
-'''
+"""
+Results
+small -> large
+top1: 0.43, 0.93; top3: 0.60, 0.97; top5: 0.70, 0.97
+all -> all
+top1: 0.48, 0.92; top3: 0.64, 0.95; top5: 0.79, 0.97
+large -> large
+top1: 0.50, 0.93; top3: 0.63, 0.97; top5: 0.80, 0.97
+"""
